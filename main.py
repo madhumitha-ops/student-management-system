0e4072edc7b7433c015a92fd8e4d47fb305f7791
Madhumitha Madduri <madhumithamadduri@gmail.com>
2026-05-28 10:22:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,17 +20,31 @@ def view_students():
     for row in records:
         print(row)
     conn.close()
+def delete_student():
+    student_id = input("Enter Student ID to Delete: ")
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "DELETE FROM students WHERE id=?",
+        (student_id,)
+    )
+    conn.commit()
+    conn.close()
+    print("Student Deleted Successfully")
 while True:
     print("\n--- Student Management System ---")
     print("1. Add Student")
     print("2. View Students")
-    print("3. Exit")
+    print("3. Delete Student")
+    print("4. Exit")
     choice = input("Enter choice: ")
     if choice == "1":
         add_student()
     elif choice == "2":
         view_students()
     elif choice == "3":
+        delete_student()
+    elif choice == "4":
         print("Thank You")
         break
     else:
