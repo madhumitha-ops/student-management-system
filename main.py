20aabe85a429b803b95a23a33386388cc2212a1d
Madhumitha Madduri <madhumithamadduri@gmail.com>
2026-05-28 10:16:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-printf("Student Management System")
+import sqlite3
+def add_student():
+    name = input("Enter Name: ")
+    age = input("Enter Age: ")
+    course = input("Enter Course: ")
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO students(name, age, course) VALUES(?,?,?)",
+        (name, age, course)
+    )
+    conn.commit()
+    conn.close()
+    print("Student Added Successfully")
+def view_students():
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM students")
+    records = cursor.fetchall()
+    for row in records:
+        print(row)
+    conn.close()
+while True:
+    print("\n--- Student Management System ---")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Exit")
+    choice = input("Enter choice: ")
+    if choice == "1":
+        add_student()
+    elif choice == "2":
+        view_students()
+    elif choice == "3":
+        print("Thank You")
+        break
+    else:
+        print("Invalid Choice")
